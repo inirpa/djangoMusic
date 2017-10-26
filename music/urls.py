bb2daa7478d3f7b3d893e935e84b64d376a0d1b6
nirparai <nirparai@yahoo.com>
2017-10-26 17:41:19 +0545

    thursday

--- music/urls.py
+++ music/urls.py
@@ -5,11 +5,14 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-	url(r'^$',views.index,name='index'),
+	url(r'^$',views.IndexView.as_view(),name='index'),
 
 	#/music/id
-	url(r'^(?P<album_id>[0-9]+)$',views.detail,name='detail'),
+	url(r'^(?P<pk>[0-9]+)$',views.DetailView.as_view(),name='detail'),
 
 	#/music/favorite
-	url(r'^(?P<album_id>[0-9]+)/favorite$',views.favorite,name='favorite'),
+	# url(r'^(?P<album_id>[0-9]+)/favorite$',views.favorite,name='favorite'),
+	url(r'album/add/$',views.AlbumCreate.as_view(),name='album-add'),
+	url(r'^album/(?P<pk>[0-9]+)$',views.AlbumUpdate.as_view(),name='album-update'),
+	url(r'^album/(?P<pk>[0-9]+)/delete/$',views.AlbumDelete.as_view(),name='album-delete'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,51 +1,29 @@
-from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-# from django.template import loader
+from django.views import generic
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.core.urlresolvers import reverse_lazy
+from .models import Album
+
+class IndexView(generic.ListView):
+	template_name = 'music/index.html'
+	context_object_name = 'all_albums'
+
+	def get_queryset(self):
+		return Album.objects.all()
 
 
-from . models import Album,Song
+class DetailView(generic.DetailView):
+	model = Album
+	template_name = 'music/detail.html'
 
-# Create your views here.
-def index(request):
-	all_albums = Album.objects.all()
-	# template = loader.get_template('music/index.html') combined with render
-	context = {
-		'all_albums' : all_albums,
-	}
-	# html = ''
-	# for album in all_albums:
-	# 	url = '/music/'+str(album.id)
-	# 	html += '<a href = "'+url+'">'+album.album_title+'</a><br>'
-	# return HttpResponse(template.render(context,request))
-	return render(request,'music/index.html',context)
+class AlbumCreate(CreateView):
+	model = Album
+	fields = ['artist','album_title','genre','album_logo']
 
-def detail(request,album_id):
-	#try:
-	# 	album = Album.objects.get(pk = album_id)
-	# 	return render(request,'music/detail.html',{'album' : album})
-	# except Album.DoesNotExist:
-	# 	raise Http404("Album doesn not exists")
-	album =  get_object_or_404(Album, pk = album_id)
-	return render(request,'music/detail.html',{'album' : album})
+class AlbumUpdate(UpdateView):
+	model = Album
+	fields = ['artist','album_title','genre','album_logo']	
 
-def favorite(request,album_id):
-	album =  get_object_or_404(Album, pk = album_id)
-
-	try:
-		selected_song = album.song_set.get(pk = request.POST['song'])
-	except(KeyError,NameError):
-		return render(request,'music/detail.html',{
-			'album':album,
-			'error_message' : 'You did not select valid song',
-			})
-	else:
-		if(selected_song.is_favorite):
-			selected_song.is_favorite = False
-		else:	
-			selected_song.is_favorite = True
-		selected_song.save()	
-		return render(request,'music/detail.html',{'album' : album})
-
-
+class AlbumDelete(DeleteView):
+	model = Album
+	success_url = reverse_lazy('music:index')
 	
